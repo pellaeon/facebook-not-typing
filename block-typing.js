dce16102ae4f7a9b41da9759fc7f7d4e08e65968
Pellaeon Lin <nfsmwlin@gmail.com>
2017-06-04 09:27:27 +0800

    Add url

--- block-typing.js
+++ block-typing.js
@@ -7,8 +7,10 @@ chrome.webRequest.onBeforeRequest.addListener(
 		{
 			urls: [
 				"https://www.facebook.com/ajax/typeahead/*",
+				"https://www.facebook.com/typeahead/*",
 				"https://www.facebook.com/ufi/typing/*",
-				"https://pixel.facebook.com/ajax/chat/opentab_tracking.php*",
+				"https://pixel.facebook.com/ajax/chat/opentab_tracking.php",
+				"https://pixel.facebook.com/ajax/log_ticker_render.php",
 				"https://www.facebook.com/ajax/messaging/typ.php*",
 				"https://www.facebook.com/ai.php*",
 				"https://*.fna.fbcdn.net/v*"
